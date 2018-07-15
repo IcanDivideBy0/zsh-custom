2f1e61181b6d42f0c8cfb6949e2ddc845a50e0be
Samuel Hurel <samuel.hurel@gmail.com>
2018-07-15 18:06:58 +0200

    Update to a more basic settings

--- README.md
+++ README.md
@@ -17,7 +17,6 @@ cd ~/.oh-my-zsh/custom
 git init
 git remote add -f origin git@github.com:IcanDivideBy0/zsh-custom.git
 git checkout master
-git submodule update --init
 cd -
 ```
 
--- env.zsh
+++ env.zsh
@@ -1,16 +1,5 @@
 # Node.js
 export NODE_ENV=development
 
-# PostgreSQL
-export PGHOST=localhost
-
-# Ansible
-export PATH="$PATH:$HOME/.local/bin"
-
 # Android & React Native
-export ANDROID_HOME=$HOME/Workspace/lib/android-sdk-linux
-export PATH=$PATH:$ANDROID_HOME/platform-tools
 export REACT_EDITOR=atom
-
-# Go
-export GOPATH=$HOME/Workspace/lib/go
